5b8beeb166b35678f7bb2fa2de37ac803623447e
xiiwingiix <xiiwingiix@daum.net>
2019-12-23 16:33:09 +0900

    update

--- hello-react/src/components/Main.js
+++ hello-react/src/components/Main.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import './Main.css'
+import './Main.css';
 
 class Main extends React.Component {
+
     render(){
         return(
             <React.Fragment>
-                <div>
+                <div className="swiper-container tmpProtection">
                     <h1 className="main_tit">보호가 필요해요!</h1>
-                    <ul>
-                        <li>
-                            <div className="img"></div>
-                            <div className="tit">페르시안 </div>
+                    <ul className="swiper-wrapper">
+                        <li className="swiper-slide">
+                            <div className="img" style={{backgroundImage:"url(https://i.pinimg.com/564x/ba/f4/4c/baf44ce74e8458729116c5e69ae2c48c.jpg)"}}></div>
+                            <div className="area">경기</div>
+                        </li>
+                        <li className="swiper-slide">
+                            <div className="img" style={{backgroundImage:"url(https://i.pinimg.com/564x/ba/f4/4c/baf44ce74e8458729116c5e69ae2c48c.jpg)"}}></div>
+                            <div className="area">경기</div>
+                        </li>
+                        <li className="swiper-slide">
+                            <div className="img" style={{backgroundImage:"url(https://i.pinimg.com/564x/ba/f4/4c/baf44ce74e8458729116c5e69ae2c48c.jpg)"}}></div>
+                            <div className="area">경기</div>
+                        </li>
+                        <li className="swiper-slide">
+                            <div className="img" style={{backgroundImage:"url(https://i.pinimg.com/564x/ba/f4/4c/baf44ce74e8458729116c5e69ae2c48c.jpg)"}}></div>
+                            <div className="area">경기</div>
+                        </li>
+                        <li className="swiper-slide">
+                            <div className="img" style={{backgroundImage:"url(https://i.pinimg.com/564x/ba/f4/4c/baf44ce74e8458729116c5e69ae2c48c.jpg)"}}></div>
+                            <div className="area">경기</div>
+                        </li>
+                        <li className="swiper-slide">
+                            <div className="img" style={{backgroundImage:"url(https://i.pinimg.com/564x/ba/f4/4c/baf44ce74e8458729116c5e69ae2c48c.jpg)"}}></div>
+                            <div className="area">경기</div>
                         </li>
                     </ul>
                 </div>
